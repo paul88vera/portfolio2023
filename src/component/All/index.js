34ec3de1ea74165dedeb89e8d4879b13339d28c8
paul88vera <paul88vera@gmail.com>
2023-02-08 23:47:46 -0600

    added project cbcmti

--- src/component/All/index.js
+++ src/component/All/index.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Images from "../../constants/images";
 
 const All = () => {
+  const [showInfo, setShowInfo] = useState(false);
   const [showInfo1, setShowInfo1] = useState(false);
   const [showInfo2, setShowInfo2] = useState(false);
   const [showInfo3, setShowInfo3] = useState(false);
@@ -12,91 +13,185 @@ const All = () => {
 
   return (
     <section id="All">
-      <div className="collage-card" onMouseEnter={() => {
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
+          setShowInfo(true);
+        }}
+        onMouseLeave={() => {
+          setShowInfo(false);
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.MTI}
+          alt="CBC Ministry Training Institute"
+          width="300"
+        />
+        {showInfo && (
+          <div className="appear">
+            <a href="http://ministrytraining.institute/" target="_blank">
+              CBC Ministry Training Institute
+            </a>
+            <p>REACT | CSS | WORDPRESS</p>
+          </div>
+        )}
+      </div>
+
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
           setShowInfo1(true);
         }}
         onMouseLeave={() => {
           setShowInfo1(false);
-        }}>
-        <img className="collage-img" src={Images.Champ} alt="CBC Champion Plaza" width="300" />
-        {showInfo1 && <div className="appear">
-          <a href="https://championplaza.com/" target="_blank">CBC Champion Plaza</a>
-          <p>HTML | CSS | JS</p>
-        </div>}
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Champ}
+          alt="CBC Champion Plaza"
+          width="300"
+        />
+        {showInfo1 && (
+          <div className="appear">
+            <a href="https://championplaza.com/" target="_blank">
+              CBC Champion Plaza
+            </a>
+            <p>HTML | CSS | JS</p>
+          </div>
+        )}
       </div>
 
-      <div className="collage-card" onMouseEnter={() => {
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
           setShowInfo2(true);
         }}
         onMouseLeave={() => {
           setShowInfo2(false);
-        }}>
-        <img className="collage-img" src={Images.Days} alt="CBC 21 Days Fasting Guide" width="300" />
-        {showInfo2 && <div className="appear">
-          <a href="https://www.communitybible.com/21days" target="_blank">
-            CBC 21 Days of Fasting Guide
-          </a>
-          <p>WordPress | CSS | JS</p>
-        </div>}
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Days}
+          alt="CBC 21 Days Fasting Guide"
+          width="300"
+        />
+        {showInfo2 && (
+          <div className="appear">
+            <a href="https://www.communitybible.com/21days" target="_blank">
+              CBC 21 Days of Fasting Guide
+            </a>
+            <p>WordPress | CSS | JS</p>
+          </div>
+        )}
       </div>
 
-      <div className="collage-card" onMouseEnter={() => {
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
           setShowInfo3(true);
         }}
         onMouseLeave={() => {
           setShowInfo3(false);
-        }}>
-        <img className="collage-img" src={Images.Leads} alt="CBC LeadsTV" width="300" />
-        {showInfo3 && <div className="appear">
-          <a href="https://www.communitybible.com/leadstv-test123455/" target="_blank">
-            CBC LeadTV
-          </a>
-          <p>WordPress | CSS | JS</p>
-        </div>}
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Leads}
+          alt="CBC LeadsTV"
+          width="300"
+        />
+        {showInfo3 && (
+          <div className="appear">
+            <a
+              href="https://www.communitybible.com/leadstv-test123455/"
+              target="_blank"
+            >
+              CBC LeadTV
+            </a>
+            <p>WordPress | CSS | JS</p>
+          </div>
+        )}
       </div>
 
-      <div className="collage-card" onMouseEnter={() => {
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
           setShowInfo4(true);
         }}
         onMouseLeave={() => {
           setShowInfo4(false);
-        }}>
-        <img className="collage-img" src={Images.Students} alt="Students page" width="300" />
-        {showInfo4 && <div className="appear">
-          <a href="https://www.communitybible.com/students/" target="_blank">
-            CBC Students Page
-          </a>
-          <p>WordPress | CSS | JS</p>
-        </div>}
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Students}
+          alt="Students page"
+          width="300"
+        />
+        {showInfo4 && (
+          <div className="appear">
+            <a href="https://www.communitybible.com/students/" target="_blank">
+              CBC Students Page
+            </a>
+            <p>WordPress | CSS | JS</p>
+          </div>
+        )}
       </div>
 
-      <div className="collage-card" onMouseEnter={() => {
+      <div
+        className="collage-card"
+        onMouseEnter={() => {
           setShowInfo5(true);
         }}
         onMouseLeave={() => {
           setShowInfo5(false);
-        }}>
-        <img className="collage-img" src={Images.Christmas} alt="CBC Christmas" width="300" />
-        {showInfo5 && <div className="appear">
-          <a href="http://www.cbcchristmas.com" target="_blank">CBC Christmas</a>
-          <p>HTML | CSS | JS</p>
-          </div>}
-        </div>
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Christmas}
+          alt="CBC Christmas"
+          width="300"
+        />
+        {showInfo5 && (
+          <div className="appear">
+            <a href="http://www.cbcchristmas.com" target="_blank">
+              CBC Christmas
+            </a>
+            <p>HTML | CSS | JS</p>
+          </div>
+        )}
+      </div>
 
-        <div className="collage-card"
+      <div
+        className="collage-card"
         onMouseEnter={() => {
           setShowInfo7(true);
         }}
         onMouseLeave={() => {
           setShowInfo7(false);
-        }}>
-        <img className="collage-img" src={Images.Social} alt="Social Network API" width="300" />
-        {showInfo7 && <div className="appear">
-          <a href="https://www.github.com/paul88vera/social-network-api" target="_blank">
-            Social Network API
-          </a>
-          <p>JS | GraphQL | Backend</p>
-        </div>}
+        }}
+      >
+        <img
+          className="collage-img"
+          src={Images.Social}
+          alt="Social Network API"
+          width="300"
+        />
+        {showInfo7 && (
+          <div className="appear">
+            <a
+              href="https://www.github.com/paul88vera/social-network-api"
+              target="_blank"
+            >
+              Social Network API
+            </a>
+            <p>JS | Express | Mongoose | Backend</p>
+          </div>
+        )}
       </div>
     </section>
   );
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,21 +1,23 @@
-import Christmas from '../img/cbcChristmas.png';
-import Leads from '../img/cbcLeadsTV.png';
-import Students from '../img/cbcStudents.png';
-import TenFour from '../img/10-4ww.png';
-import Days from '../img/21days.gif';
-import Champ from '../img/ChampionPlaza.jpg';
-import Circulize from '../img/Circulize.png';
-import Social from '../img/socialAPI.png';
-import Me from '../img/PaulVeraCropped.png';
+import Christmas from "../img/cbcChristmas.png";
+import Leads from "../img/CBCLTV.png";
+import Students from "../img/cbcStudents.png";
+import TenFour from "../img/10-4ww.png";
+import Days from "../img/21days.gif";
+import Champ from "../img/ChampionPlaza.jpg";
+import MTI from "../img/CBCMTI.jpg";
+import Circulize from "../img/Circulize.png";
+import Social from "../img/SocialAPI.png";
+import Me from "../img/PaulVeraCropped.png";
 
 export default {
-  Christmas, 
-  Leads, 
+  Christmas,
+  Leads,
   Students,
   TenFour,
   Days,
   Champ,
+  MTI,
   Circulize,
   Social,
-  Me
-}
+  Me,
+};
